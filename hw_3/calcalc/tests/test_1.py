6ac2da9dbcd118378d8b19337e1d0186341e7cec
Jacqueline Beechert <jbeechert56@gmail.com>
2022-02-15 09:30:30 -0800

    restoring this file after accidental re-write

--- hw_3/calcalc/tests/test_1.py
+++ hw_3/calcalc/tests/test_1.py
@@ -1,32 +1,22 @@
-name: Python package
+from CalCalc import calculate
 
-on: [push]
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    strategy:
-      # You can use PyPy versions in python-version.
-      # For example, pypy2 and pypy3
-      matrix:
-        python-version: ["3.9"]
-
-    steps:
-      - uses: actions/checkout@v2
-      - name: Set up Python ${{ matrix.python-version }}
-        uses: actions/setup-python@v2
-        with:
-          python-version: ${{ matrix.python-version }}
-      # You can test your matrix by printing the current Python version
-      - name: Display Python version
-        run: python -c "import sys; print(sys.version)"
-      # Install dependencies
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          python -m pip install pytest
-          pip install -r requirements.txt
-        working-directory: hw_3
-      - name: Test and Generate coverage report
-        run: |
-          python -m pytest 
+def test_1():
+    assert abs(100.0 - calculate('1000/10')) < 0.001
+    
+def test_2():
+    assert calculate('How many ounces are in a gallon', True) == '128 fluid ounces' 
+    
+def test_3():
+    expression = '2 + 3 + 6**2'
+    eval_answer = float(calculate(expression))
+    wolfram_answer = float(calculate(expression, True))
+    assert abs(eval_answer - wolfram_answer) < 0.001
+    
+def test_4():
+    # test that 'numpy' works with inf
+    assert calculate('np.inf/100') == float('inf')
+    
+def test_5():
+    # test that 'math' works with unit circle area
+    area = 'math.pi*1**2'
+    assert abs(3.14159 - calculate(area)) < 0.001
